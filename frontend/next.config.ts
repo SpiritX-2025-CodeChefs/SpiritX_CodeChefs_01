6292664b0e5028a587d9adf9101f76d77e085aa8
Yasoga Nanayakkarawasam <yasogan6@outlook.com>
2025-03-08 11:31:22 +0530

    Add API URL configuration for development and production environments

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: (!!process && process.env.NODE_ENV === "development") ? 'http://localhost:8000/:path*' : `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
+      },
+    ];
+  }
 };
 
 export default nextConfig;
