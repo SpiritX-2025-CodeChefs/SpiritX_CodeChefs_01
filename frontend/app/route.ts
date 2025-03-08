8310d686a4e848a1b4b397098812a8612819af8d
Yasoga Nanayakkarawasam <yasogan6@outlook.com>
2025-03-08 09:19:51 +0530

    Remove default home page and add session validation route

---
+++ frontend/app/route.ts
@@ -0,0 +1,25 @@
+import { upfetch } from "@/lib/upfetch";
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+
+export async function GET(req: NextRequest) {
+  try {
+    const response = await upfetch(`/api/validate-session`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      schema: z.object({
+        success: z.boolean(),
+      }),
+    })
+    if (response.success) {
+      return NextResponse.redirect(new URL("/dashboard", req.url))
+    } else {
+      return NextResponse.redirect(new URL("/login", req.url))
+    }
+  } catch {
+    return NextResponse.redirect(new URL("/login", req.url))
+  }
+}
