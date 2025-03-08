b4c05aa6b69622abe52e7854a47d934ea53eb919
Yasoga Nanayakkarawasam <yasogan6@outlook.com>
2025-03-08 11:17:36 +0530

    Add dark mode

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { ThemeProvider } from "@/components/theme-provider"
 import "./globals.css";
+import { ModeToggle } from "@/components/ui/switcher";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,11 +25,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+          <div className="fixed bottom-5 right-5">
+            <ModeToggle />
+          </div>
+        </ThemeProvider>
       </body>
     </html>
   );
